2d2c48d84cd9c0d4be022c3320ca1ab2baf56e1d
Nick <nickrange@gmail.com>
2013-03-13 13:57:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SapleApp::Application.config.secret_token = 'cfef1c4870de3bece7f0788994de717a4e40767ca560581d97fddd7d146a0f504b195c6a1f912e06fcd7c0342016388bb18e2e64f64adbebc7850cb9123c1429'
