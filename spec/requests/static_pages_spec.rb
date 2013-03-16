25fae4e90e0e1dd9c4aea655321cb8d2f340a395
Nick <nickrange@gmail.com>
2013-03-16 09:54:47 +0000

    Adding the link checks to the spec file for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,4 +47,29 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
    end
+   
+   # links
+   it "should have the right links on the layout" do
+     visit root_path
+     click_link "About"
+     page.should have_selector 'title', text: full_title('About Us')
+   
+     visit root_path
+     click_link "Contact"
+     page.should have_selector 'title', text: full_title('Contact Us')
+
+     visit root_path
+     click_link "Help"
+     page.should have_selector 'title', text: full_title('Help')
+
+     visit root_path
+     click_link "Sign up now!"
+     page.should have_selector 'title', text: full_title('Sign up')
+
+     visit root_path
+     click_link "sample app"
+     page.should have_selector 'title', text: full_title( '' )		 
+    
+   end
+
 end
