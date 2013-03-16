bbeece9481309e3a191b7f727b3951cdb8b12378
Nick <nickrange@gmail.com>
2013-03-16 09:31:44 +0000

    updating the spec for static pages, simplifying it, adding common elements

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,50 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  # setup and common behaviour
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title) )} # this uses a helper function
+  end
+  
+  # homepage
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
+    # set the variables for the common tests
+    let(:heading)    { 'Welcome to the Sample App' }
+    let(:page_title) { "" }
+    
+    it_should_behave_like "all static pages"                        # call the common test
+    it { should_not have_selector 'title', text: '| Home' }         # text was removed
   end
-
+  # help page
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    
+    it_should_behave_like "all static pages"
+     
   end
-
+  # about page
   describe "About page" do
     before { visit about_path }
+    
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
-  end
-
+    it_should_behave_like "all static pages"
+   end
+  # contact page
   describe "Contact page" do
     before { visit contact_path }
+    
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
-  end
+    it_should_behave_like "all static pages"
+   end
 end
